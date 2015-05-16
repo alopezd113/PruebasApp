41466e8ea80fc3fd8b848ae550dc867c6d7a6055
usuario <usuario@xubunt>
2015-05-17 01:37:29 +0200

    dadsf

---
+++ src/application/Controlador.java
@@ -0,0 +1,26 @@
+package application;
+
+import java.awt.Button;
+
+import javafx.fxml.FXML;
+import javafx.scene.control.Label;
+import javafx.scene.control.TextField;
+
+public class Controlador {
+
+	 @FXML
+	    private TextField password;
+
+	    @FXML
+	    private Label usuario;
+
+	    @FXML
+	    private TextField codprofe;
+
+	    @FXML
+	    private Label contraseña;
+
+	    @FXML
+	    private Button button;
+
+}
