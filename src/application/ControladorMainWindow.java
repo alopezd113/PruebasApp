32dd8fa1c12d9a2df880c2f95965a4f1d03b8073
usuario <usuario@xubunt>
2015-05-19 20:28:54 +0200

    19 mayo

---
+++ src/application/ControladorMainWindow.java
@@ -0,0 +1,22 @@
+package application;
+
+import javafx.fxml.FXML;
+import javafx.scene.control.Accordion;
+import javafx.scene.control.TitledPane;
+
+public class ControladorMainWindow {
+
+	 @FXML
+	    private Accordion menu;
+
+	    @FXML
+	    private TitledPane horarios;
+
+	    @FXML
+	    private TitledPane faltas;
+
+	    @FXML
+	    private TitledPane guardias;
+	
+	
+}
