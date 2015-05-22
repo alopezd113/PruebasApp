49123b28eacd26a8da0825d75c8a2cc70e646fb3
usuario <usuario@xubunt>
2015-05-22 11:18:49 +0200

    21_05

--- src/application/ControladorMainWindow.java
+++ src/application/ControladorMainWindow.java
@@ -1,22 +1,74 @@
 package application;
 
+import java.net.URL;
+import java.util.ResourceBundle;
 import javafx.fxml.FXML;
 import javafx.scene.control.Accordion;
+import javafx.scene.control.Button;
 import javafx.scene.control.TitledPane;
 
 public class ControladorMainWindow {
 
-	 @FXML
-	    private Accordion menu;
+    @FXML
+    private ResourceBundle resources;
 
-	    @FXML
-	    private TitledPane horarios;
+    @FXML
+    private URL location;
 
-	    @FXML
-	    private TitledPane faltas;
+    @FXML
+    private Accordion menu;
 
-	    @FXML
-	    private TitledPane guardias;
-	
-	
+    @FXML
+    private TitledPane horarios;
+
+    @FXML
+    private Button verHorarios;
+
+    @FXML
+    private TitledPane faltas;
+
+    @FXML
+    private Button comunicarFalta;
+
+    @FXML
+    private Button verFaltas;
+
+    @FXML
+    private TitledPane guardias;
+
+    @FXML
+    private Button firmarGuardia;
+
+    @FXML
+    private Button verGuardias;
+
+    @FXML
+    private Button asignarGuardias;
+
+    @FXML
+    private TitledPane informes;
+
+    @FXML
+    private Button BIGuardias;
+
+    @FXML
+    private Button BIFaltas;
+
+    @FXML
+    void initialize() {
+        assert menu != null : "fx:id=\"menu\" was not injected: check your FXML file 'MainWindow.fxml'.";
+        assert horarios != null : "fx:id=\"horarios\" was not injected: check your FXML file 'MainWindow.fxml'.";
+        assert verHorarios != null : "fx:id=\"verHorarios\" was not injected: check your FXML file 'MainWindow.fxml'.";
+        assert faltas != null : "fx:id=\"faltas\" was not injected: check your FXML file 'MainWindow.fxml'.";
+        assert comunicarFalta != null : "fx:id=\"comunicarFalta\" was not injected: check your FXML file 'MainWindow.fxml'.";
+        assert verFaltas != null : "fx:id=\"verFaltas\" was not injected: check your FXML file 'MainWindow.fxml'.";
+        assert guardias != null : "fx:id=\"guardias\" was not injected: check your FXML file 'MainWindow.fxml'.";
+        assert firmarGuardia != null : "fx:id=\"firmarGuardia\" was not injected: check your FXML file 'MainWindow.fxml'.";
+        assert verGuardias != null : "fx:id=\"verGuardias\" was not injected: check your FXML file 'MainWindow.fxml'.";
+        assert asignarGuardias != null : "fx:id=\"asignarGuardias\" was not injected: check your FXML file 'MainWindow.fxml'.";
+        assert informes != null : "fx:id=\"informes\" was not injected: check your FXML file 'MainWindow.fxml'.";
+        assert BIGuardias != null : "fx:id=\"BIGuardias\" was not injected: check your FXML file 'MainWindow.fxml'.";
+        assert BIFaltas != null : "fx:id=\"BIFaltas\" was not injected: check your FXML file 'MainWindow.fxml'.";
+
+    }
 }
